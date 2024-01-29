5f882ab7679de5320f9e6db4b7ad9d74866a811a
Eli Sachs <easachs13@gmail.com>
2024-01-29 13:40:59 -0700

    Feat: experiment w advanced search options dropdown

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -3,7 +3,11 @@
 module HomeHelper
   def count_options = (1..5).to_a.map { |n| [n, n] }
 
-  def sort_options = [['Best match', 'best'], ['Rating', 'rating'], ['Distance', 'distance']]
+  def sort_options
+    [['Best match', 'best'],
+     ['Rating', 'rating'],
+     ['Distance', 'distance']]
+  end
 
   def lodging_data
     [['B & B', 'bed and breakfast'], ['Campsite', 'campsites'],
