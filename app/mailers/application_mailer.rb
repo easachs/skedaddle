d19a5b0f649e300898d088373d31eb90d7353ec7
Eli Sachs <easachs13@gmail.com>
2024-12-20 12:25:42 -0700

    Feat: fix contact email

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: ENV.fetch('MAILER_FROM_ADDRESS', 'easachs13@gmail.com')
   layout 'mailer'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,6 +22,18 @@ Rails.application.configure do
   config.cache_store = :redis_cache_store, { url: ENV.fetch('REDIS_URL', nil) }
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    domain: 'skedaddle.herokuapp.com',
+    user_name: 'apikey',
+    password: ENV.fetch('SENDGRID_API_KEY', nil),
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
+
+  config.action_mailer.default_url_options = { host: 'skedaddle.herokuapp.com' }
 
   config.i18n.fallbacks = true
 
